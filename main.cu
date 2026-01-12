b3b653948d93fb84fed316c08e30b8a4139494a4
Sriram Katta <sriram.katta@fau.de>
2026-01-12 23:55:13 +0100

    Refactor realMain to integrate MPI and NCCL for improved multi-GPU support; enhance memory management and initialization processes.

--- main.cu
+++ main.cu
@@ -1,80 +1,131 @@
 // V2 all gpu kernls only data moved once in each diection
 #include "cg-util.h"
 #include "kernel.cuh"
+#include "mpi_comm.hpp"
+#include "nccl_comm.hpp"
 
 #include <fmt/format.h>
 #include <string_view>
 
 #include <gcxx/api.hpp>
 
+#define NCCL_USE 1
 
 template <typename VT>
 inline int realMain(int argc, char *argv[]) {
-  auto [tpeName, nx, ny, nItWarmUp, nIt] = parseCLA_2d(argc, argv);
+
+  auto world_comm = mpicomm::world();
+  int world_rank = world_comm.rank();
+  int world_size = world_comm.size();
+
+  auto devcount = gcxx::Device::count();
+
+  if (world_size != devcount) {
+    fmt::print("we need the device count to be equal to number of ranks");
+    std::exit(EXIT_FAILURE);
+  }
+
+  int localrank = -1;
+  int localsize = 0;
+  {
+    auto localcomm =
+      mpicomm::split_type(world_comm, MPI_COMM_TYPE_SHARED, world_rank);
+    localrank = localcomm.rank();
+    localsize = localcomm.size();
+  }
+
+  ncclUniqueId ncclid{};
+  if (world_rank == 0) {
+    ncclid = nccl::getuniqueID();
+  }
+
+  world_comm.byte_bcast(&ncclid, sizeof(ncclid), 0);
+
+  world_comm.barrier();
+  auto devref = gcxx::Device::set(localrank);
+
+  ncclcomm ncomm(localrank, localsize, ncclid);
+
+  {  // scope may not be needed
+    auto [tpeName, nx, ny, nIt] = parseCLA_2d(argc, argv);
 
 
-  auto total_elems = nx * ny;
+    auto total_elems = nx * ny;
 
-  auto u_host_raii = gcxx::memory::make_host_pinned_unique_ptr<VT>(total_elems);
-  auto rhs_host_raii =
-    gcxx::memory::make_host_pinned_unique_ptr<VT>(total_elems);
+    auto u_host_raii =
+      gcxx::memory::make_host_pinned_unique_ptr<VT>(total_elems);
+    auto rhs_host_raii =
+      gcxx::memory::make_host_pinned_unique_ptr<VT>(total_elems);
 
-  VT *u_host = u_host_raii.get();
-  VT *rhs_host = rhs_host_raii.get();
+    VT *u_host = u_host_raii.get();
+    VT *rhs_host = rhs_host_raii.get();
 
-  // init
-  initConjugateGradient(u_host, rhs_host, nx, ny);
+    // init
+    initConjugateGradient(u_host, rhs_host, nx, ny);
 
-  auto u_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
-  auto rhs_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
+#if NCCL_USE
+    auto u_raii = nccl::make_nccl_unique<VT>(total_elems);
+    auto rhs_raii = nccl::make_nccl_unique<VT>(total_elems);
+#else
+    auto u_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
+    auto rhs_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
+#endif
 
-  VT *u = u_raii.get();
-  VT *rhs = rhs_raii.get();
+    VT *u = u_raii.get();
+    VT *rhs = rhs_raii.get();
 
-  gcxx::memory::Copy(u, u_host, total_elems);
-  gcxx::memory::Copy(rhs, rhs_host, total_elems);
+    gcxx::memory::Copy(u, u_host, total_elems);
+    gcxx::memory::Copy(rhs, rhs_host, total_elems);
 
 
-  auto res_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
-  auto p_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
-  auto ap_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
-  gcxx::memory::Memset(res_raii, 0, total_elems);
-  gcxx::memory::Memset(p_raii, 0, total_elems);
-  gcxx::memory::Memset(ap_raii, 0, total_elems);
-  VT *res = res_raii.get();
-  VT *p = p_raii.get();
-  VT *ap = ap_raii.get();
+#if NCCL_USE
+    auto res_raii = nccl::make_nccl_unique<VT>(total_elems);
+    auto p_raii = nccl::make_nccl_unique<VT>(total_elems);
+    auto ap_raii = nccl::make_nccl_unique<VT>(total_elems);
+#else
+    auto res_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
+    auto p_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
+    auto ap_raii = gcxx::memory::make_device_unique_ptr<VT>(total_elems);
+#endif
 
-  // warm-up
-  nItWarmUp = conjugateGradient(rhs, u, res, p, ap, nx, ny, nItWarmUp);
+    gcxx::memory::Memset(res_raii, 0, total_elems);
+    gcxx::memory::Memset(p_raii, 0, total_elems);
+    gcxx::memory::Memset(ap_raii, 0, total_elems);
+    VT *res = res_raii.get();
+    VT *p = p_raii.get();
+    VT *ap = ap_raii.get();
 
-  // measurement
-  auto start = std::chrono::steady_clock::now();
+    // warm-up
+    // nItWarmUp = conjugateGradient(rhs, u, res, p, ap, nx, ny, nItWarmUp);
 
-  nIt = conjugateGradient(rhs, u, res, p, ap, nx, ny, nIt);
+    // measurement
+    auto start = std::chrono::steady_clock::now();
 
-  auto end = std::chrono::steady_clock::now();
-  fmt::print("  CG steps:      {}\n", nIt);
+    nIt = conjugateGradient(rhs, u, res, p, ap, nx, ny, nIt);
 
-  printStats<VT>(end - start, nIt, nx * ny, tpeName, 8 * sizeof(VT), 15);
+    auto end = std::chrono::steady_clock::now();
+    fmt::print("  CG steps:      {}\n", nIt);
 
-  gcxx::memory::Copy(u_host, u, total_elems);
-  gcxx::memory::Copy(rhs_host, rhs, total_elems);
+    printStats<VT>(end - start, nIt, nx * ny, tpeName, 8 * sizeof(VT), 15);
 
-  // check solution
-  checkSolutionConjugateGradient(u_host, rhs_host, nx, ny);
+    gcxx::memory::Copy(u_host, u, total_elems);
+    gcxx::memory::Copy(rhs_host, rhs, total_elems);
 
+    // check solution
+    checkSolutionConjugateGradient(u_host, rhs_host, nx, ny);
+  }  // scope may not be needed
 
   return 0;
 }
 
 int main(int argc, char *argv[]) {
+  mpienv env(argc, argv);
 
   std::string_view tpeName;
 
   if (argc < 2) {
     fmt::print("Missing type specification using double");
-    tpeName = "double";
+    return -1;
   }
 
   tpeName = argv[1];
